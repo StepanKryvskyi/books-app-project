f7f571ad01d06cafdc6a1e734e3e02a9b1bcfccc
Mykhailo <mishamamin2016@gmail.com>
2023-09-05 19:06:08 +0200

    add dark mode button
    add function turn on and turn off dark mode

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-
-
-import { createCategoryGallery, createBestsellersGallery} from './js/books-gallery-cat';
+import {
+  createCategoryGallery,
+  createBestsellersGallery,
+} from './js/books-gallery-cat';
 import { galleryList, catList, catTitle } from './js/refs';
 import { addCategoryTitle } from './js/gallery-markup';
 import { createCategory } from './js/query-and-markup';
 import { load } from './js/support-ukraine';
+import { onBtnThemeClick } from './js/dark-mode';
 // creating list of categories
 createCategory();
 // Creating bestsellers gallery
@@ -15,18 +17,17 @@ createBestsellersGallery();
 
 catList.addEventListener('click', onClickShowCatBooks);
 
-function onClickShowCatBooks(evt) {    
-    galleryList.innerHTML = "";
-    if (evt.target === catList.firstElementChild) {
-        addCategoryTitle('Best Sellers Books');
-        createBestsellersGallery();
+function onClickShowCatBooks(evt) {
+  galleryList.innerHTML = '';
+  if (evt.target === catList.firstElementChild) {
+    addCategoryTitle('Best Sellers Books');
+    createBestsellersGallery();
   } else {
-    const cat = evt.target.textContent; 
-     addCategoryTitle(cat);
-     createCategoryGallery(cat); 
-}
-    
+    const cat = evt.target.textContent;
+    addCategoryTitle(cat);
+    createCategoryGallery(cat);
+  }
 }
 
-load()
-
+load();
+onBtnThemeClick();
