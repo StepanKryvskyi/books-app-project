94d947ce0dff9576e70078d4c2fce817a8b75847
Mykhailo <mishamamin2016@gmail.com>
2023-09-06 15:18:32 +0200

    fix dark icon

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-
 // import './js/pagination';
-import { createCategoryGallery, createBestsellersGallery} from './js/books-gallery-cat';
-import { galleryList, catList, loader, bookCard} from './js/refs';
+import {
+  createCategoryGallery,
+  createBestsellersGallery,
+} from './js/books-gallery-cat';
+import { galleryList, catList, loader, bookCard } from './js/refs';
 import { onBtnThemeClick } from './js/dark-mode';
 import { addCategoryTitle } from './js/gallery-markup';
 import { createCategory } from './js/query-and-markup';
-import {  createMarkup, createBookCard } from './js/pop-up';
+import { createMarkup, createBookCard } from './js/pop-up';
 
 // import { createShoppingCard } from './js/shopping-list';
 
 createCategoryGallery();
 // createShoppingCard();
-
-
 
 import { load } from './js/support-ukraine';
 
@@ -24,33 +24,33 @@ createBestsellersGallery();
 
 // Creating books gallery by category
 
-load()
+load();
 
 catList.addEventListener('click', onClickShowCatBooks);
 
 onBtnThemeClick();
 
-function onClickShowCatBooks(evt) {    
-  galleryList.innerHTML = "";
+function onClickShowCatBooks(evt) {
+  galleryList.innerHTML = '';
   loader.classList.toggle('visually-hidden');
-  if (evt.target.textContent === "All categories") {
+  if (evt.target.textContent === 'All categories') {
     addCategoryTitle('Best Sellers Books # #');
     createBestsellersGallery();
     loader.classList.toggle('visually-hidden');
   } else {
-    const cat = evt.target.textContent; 
+    const cat = evt.target.textContent;
     addCategoryTitle(cat);
     createCategoryGallery(cat);
     loader.classList.toggle('visually-hidden');
-}   
+  }
 }
 
-galleryList.addEventListener("click", onBookClick)
+galleryList.addEventListener('click', onBookClick);
 
- function onBookClick(evt){
+function onBookClick(evt) {
   const bookId = evt.target.closest('.book-card').getAttribute('id');
-  popUp.style.display = "block";
-  
-    createBookCard(bookId)
+  popUp.style.display = 'block';
+
+  createBookCard(bookId);
 }
-onBookClick()
+onBookClick();
