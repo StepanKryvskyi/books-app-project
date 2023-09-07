8f283291549780a016f03f3e24b93cd1c463455c
StepanKryvskyi <126700161+StepanKryvskyi@users.noreply.github.com>
2023-09-07 19:05:02 +0300

    mobil-menu

--- src/js/mobil-menu.js
+++ src/js/mobil-menu.js
@@ -4,10 +4,12 @@ const closeMenuBtn = document.querySelector('.js-close-menu');
 
 function openMenu() {
     mobileMenu.style.transform = 'translateX(0%)';
+    mobileMenu.style.display = 'block';
 }
 
 function closeMenu() {
     mobileMenu.style.transform = 'translateX(-100%)';
+    mobileMenu.style.display = 'none';
 }
 
 menuToggleBtn.addEventListener('click', openMenu);
