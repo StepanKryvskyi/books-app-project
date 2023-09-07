bfcaa680a08569bcea5cc023df96942fb7fcf676
l1nes8 <egor1glavatyje@gmail.com>
2023-09-07 14:54:42 +0300

    update code

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -5,47 +5,47 @@ const arraySopportUA = [
   {
     title: 'Save the Children',
     url:'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './img/support-img/image-1.png',
+    img: './img/support-img/image-1.png?url',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: './img/support-img/image-2.png',
+    img: './img/support-img/image-2.png?url',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: './img/support-img/image-3.png',
+    img: './img/support-img/image-3.png?url',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: './img/support-img/image-4.png',
+    img: './img/support-img/image-4.png?url',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: './img/support-img/image-5.png',
+    img: './img/support-img/image-5.png?url',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: './img/support-img/image-6.png',
+    img: './img/support-img/image-6.png?url',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: './img/support-img/image-7.png',
+    img: './img/support-img/image-7.png?url',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: './img/support-img/image-8.png',
+    img: './img/support-img/image-8.png?url',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: './img/support-img/image-9.png',
+    img: './img/support-img/image-9.png?url',
   },
 ]
 
