619f5ea9ba312b56a32c2b0dfb9cd87390228707
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-09-07 00:25:53 -0400

    some changes
    Зробила порядок у main.js, трохи підшліфувала темну тему, бо логотипу не було видно, відредагувала функцію по кліку на кнопку see more. Якщо темна тема вже зроблена повністю, то не додавай тоді мої зміни

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -1,12 +1,14 @@
+import {galleryList, btnSeeMore} from './refs'
 // Button Toggle
 const themeToggleBtn = document.querySelector('#themeToggle');
 
 // Header Item
+const logoIcon = document.querySelector('.logo-icon');
 const headerEL = document.querySelector('#header');
 const shoperIcon = document.querySelector('.shoper-icon');
 
 // Book List Item
-const bookEl = document.querySelector('.book-list');
+const bookEl = document.querySelector('.list-link');
 
 themeToggleBtn.addEventListener('change', onBtnThemeClick);
 
@@ -15,16 +17,16 @@ export function onBtnThemeClick() {
   if (themeToggleBtn.checked) {
     document.body.classList.add('darkmode');
     headerEL.classList.add('header-dark');
-
+    logoIcon.classList.add('logo-dark');
     shoperIcon.classList.add('shoper-dark');
-
     bookEl.classList.add('book-dark');
+    
   } else {
     document.body.classList.remove('darkmode');
     headerEL.classList.remove('header-dark');
-
+    logoIcon.classList.remove('logo-dark');
     shoperIcon.classList.remove('shoper-dark');
-
     bookEl.classList.remove('book-dark');
   }
 }
+
